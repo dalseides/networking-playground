4d1e3978c195aacbbef7b3a9fa8d1ba18597b66e
dalseides <13833003+dalseides@users.noreply.github.com>
2026-04-09 23:09:10 +0100

    Committing stuff that works for openVPN confirmed, and adding a readme with an important gotcha.

---
+++ README.md
@@ -0,0 +1,8 @@
+NOTICE
+
+The use of these scripts depends on the creation of the /etc/netns/vpn and /etc/netns/tor directories.
+
+In the case of the former, both /etc/resolv.conf and /etc/resolvconf.conf need to be copied into the /etc/netns/vpn directory, to avoid disrupting DNS on the host.
+
+In the case of the latter, I don't know, I haven't done the TOR part yet.
+
--- net-setup.sh
+++ net-setup.sh
@@ -1,41 +1,43 @@
 #!/bin/bash -x
 
+# create namespaces
 ip netns add vpn
-#ip netns add tor
+ip netns add tor
 
+# turn on loopback in each
 ip netns exec vpn ip link set dev lo up
-#ip netns exec tor ip link set dev lo up
+ip netns exec tor ip link set dev lo up
 
+# create and assign veth pairs
 ip link add eth-to-vpn type veth peer name vpn-to-eth
 ip link set dev vpn-to-eth netns vpn
-#ip link add eth-to-tor type veth peer name tor-to-eth
-#ip link set dev tor-to-eth netns tor
-
 ip link set dev eth-to-vpn master br0
-#ip link set dev eth-to-tor master br0
 
-# ip addr add 10.10.10.1/24 dev eth-to-vpn
-# ip addr add 10.10.100.1/24 dev eth-to-tor
-# 
-# ip netns exec vpn ip addr add 10.10.10.2/24 dev vpn-to-eth
-# ip netns exec tor ip addr add 10.10.100.2/24 dev tor-to-eth
-# 
-# ip netns exec vpn ip route add default via 10.10.10.1 dev vpn-to-eth
-# ip netns exec tor ip route add default via 10.10.100.1 dev tor-to-eth
+ip link add eth-to-tor type veth peer name tor-to-eth
+ip link set dev tor-to-eth netns tor
+ip link set dev eth-to-tor master br0
 
-ip netns exec vpn ip link set dev vpn-to-eth address 01:02:03:04:05:06
+# bring up veth cards
+ip netns exec vpn ip link set dev vpn-to-eth address 10:02:03:04:05:06
 ip netns exec vpn ip link set vpn-to-eth up
-#ip netns exec tor ip link set dev tor-to-eth address 01:02:03:04:05:90
-#ip netns exec tor ip link set tor-to-eth up
-
+ip link set dev eth-to-vpn address 10:20:30:40:50:60
 ip link set dev eth-to-vpn up
-#ip link set dev eth-to-tor up
 
+ip netns exec tor ip link set dev tor-to-eth address 10:07:08:09:11:12
+ip netns exec tor ip link set tor-to-eth up
+ip link set dev eth-to-tor address 10:22:33:44:55:66
+ip link set dev eth-to-tor up
+
+# enable ICMP in each namespace
 ip netns exec vpn sysctl -w net.ipv4.ping_group_range="0 1000"
-#ip netns exec tor sysctl -w net.ipv4.ping_group_range="0 1000"
+ip netns exec tor sysctl -w net.ipv4.ping_group_range="0 1000"
 
-ip netns exec vpn dhcpcd -n vpn-to-eth -v
-ip netns exec vpn openvpn us-free-10.protonvpn.tcp.ovpn &
+# get an IP address for each namespace
+ip netns exec vpn dhcpcd -n vpn-to-eth
+ip netns exec tor dhcpcd -n tor-to-eth
 
-#ip netns exec tor dhcpcd -n tor-to-eth -v
+# run OpenVPN in the vpn namespace
+ip netns exec vpn openvpn /root/us-free-10.protonvpn.tcp.ovpn &
+
+# run TOR in the tor namespace
 
